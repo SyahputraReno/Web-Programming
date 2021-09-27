311584ecbc81cd0e53d20a6c99e8c0a1a0585006
SyahputraReno <90619423+SyahputraReno@users.noreply.github.com>
2021-09-27 21:38:11 +0700

    1
    2

---
+++ application/controllers/Pertemuan1.php
@@ -0,0 +1,13 @@
+<?php 
+class Pertemuan1 extends CI_Controller {
+    public function index()
+    {
+        echo"<h1> Halo Perkenalkan </h1>";
+        echo"Nama saya Reno Budi Syahputra <br>
+        Saya tinggal di Bekasi, Tambun Selatan <br>
+        Saat ini saya berkuliah di Bina Sarana Informatika dan Memasuki Semester 3 <br>
+        Hobi saya adalah memotret"; 
+    }
+}
+
+?>
